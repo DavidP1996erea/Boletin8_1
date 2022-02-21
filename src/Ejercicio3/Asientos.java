f4c8a63d9062f0bc3702c6d40d706d2222893d43
David Perea <david.perea@iesnervion.es>
2022-02-21 23:23:09 +0100

    ejercicio3

--- src/Ejercicio3/Asientos.java
+++ src/Ejercicio3/Asientos.java
@@ -1,11 +1,112 @@
 package Ejercicio3;
-
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Asientos {
-    private char fumadores;
 
-    public static void introducirPasajeros() {
+    private static int fumadores = 16;
+    private static int noFumadores = 0;
+
+    public Asientos() {
+
+    }
+
+
+    public static void introducirPasajeros(int[] pasajeros) {
+        Scanner sc = new Scanner(System.in);
+        char respuesta;
+
+
+        do {
+
+            if (esFumador() == true) {
+
+                asientoFumador(pasajeros);
+
+            } else {
+                asientoNoFumador(pasajeros);
+            }
+
+
+            System.out.println("¿Desea introducir más personas?");
+            respuesta = sc.next().toUpperCase(Locale.ROOT).charAt(0);
+
+        } while (respuesta == 'S');
+    }
+
+
+
+
+
+    public static void asientoFumador(int[] pasajeros) {
+        Scanner sc = new Scanner(System.in);
+        char respuesta;
+
+        if (fumadores <= 19) {
+            pasajeros[fumadores] = 1;
+            fumadores++;
+        } else {
+
+            System.out.println("¿Desea pasar a la zona de no fumadores?");
+            respuesta = sc.next().toUpperCase(Locale.ROOT).charAt(0);
+
+            if (respuesta == 'S') {
+                if (noFumadores < 16) {
+
+                    asientoNoFumador(pasajeros);
+
+                }
+                }else {
+                System.out.println("El próximo vuelo es mañana");
+            }
+        }
+    }
+
+
+    private static void asientoNoFumador(int[] pasajeros) {
+
+        Scanner sc = new Scanner(System.in);
+        char respuesta;
+
+        if (noFumadores < 16) {
+
+            pasajeros[noFumadores] = 1;
+            noFumadores++;
+
+        } else {
+
+            System.out.println("¿Desea pasar a la zona de fumadores?");
+            respuesta = sc.next().toUpperCase(Locale.ROOT).charAt(0);
+
+            if (respuesta == 'S') {
+                asientoFumador(pasajeros);
+            } else {
+                System.out.println("El próximo vuelo es mañana");
+            }
+        }
 
 
     }
-}
+
+
+
+
+    public static boolean esFumador(){
+        Scanner sc = new Scanner(System.in);
+        boolean fumador=false;
+
+        System.out.println("Introduzca 'S' si el pasajaero es fumador o 'N' si no lo es");
+        char respuesta=sc.next().toUpperCase(Locale.ROOT).charAt(0);
+
+        if(respuesta=='S'){
+            fumador=true;
+        }
+
+        return fumador;
+
+    }
+
+
+    } //fin
+
+
--- src/Ejercicio3/ejercicio3.java
+++ src/Ejercicio3/ejercicio3.java
@@ -6,7 +6,16 @@ public class ejercicio3 {
     public static void main(String[] args) {
 
 
+        int [] asientos= new int [20];
 
+
+        introducirPasajeros(asientos);
+
+        for(int x: asientos){
+
+            System.out.println(x);
+
+        }
     }
 
 }
